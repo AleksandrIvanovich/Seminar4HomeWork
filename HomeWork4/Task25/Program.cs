ed4247b3830c04383e235fb4be7929bdf28235ca
Aleksandr <fomichov73@mail.ru>
2022-10-02 13:49:46 +0300

    Added task solution 29

---
+++ HomeWork4/Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+int GetNumber(string message)
+ {
+    int result = 0;
+    bool correctInput = false;
+    while(!correctInput)
+        {         
+            Console.WriteLine(message);         
+            correctInput = int.TryParse(Console.ReadLine(), 
+            out result); 
+
+            if(result < 1)             
+                correctInput = false;  
+
+            if(!correctInput)             
+                Console.WriteLine("Вы ввели некорректное число");          
+                Console.WriteLine();     
+        }      
+        return result; 
+ }      
+
+Console.WriteLine("Введите первое число:"); 
+int numA = int.Parse(Console.ReadLine() ?? "");
+ 
+Console.WriteLine("Введите второе число:");
+int numB = int.Parse(Console.ReadLine()??"");
+
+double result = Math.Pow(numA, numB);
+
+Console.WriteLine($"Число {numA} возведенное в степень {numB} равно: {result}");
